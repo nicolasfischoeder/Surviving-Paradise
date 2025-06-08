f8211a0ad927489511abc6a3dec4414532946a54
Nicolas Fischöder <nicolasfischoeder@gmail.com>
2025-06-08 19:54:45 +0200

    Initial Project

---
+++ Source/SurvivingParadise1/SurvivingParadise1GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurvivingParadise1GameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASurvivingParadise1GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASurvivingParadise1GameMode();
+};
+
+
+
